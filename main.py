7ecc11d3b2a38ca2f66c2f9f8079918a9291c6a7
30694 <kostaskotrotsos@gmail.com>
2023-03-16 13:51:16 +0200

    SimpleSymbols

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello wolrd")
+def SimpleSymbols(str1):
+    str2 = str1
+    index = 0
+    print("l",len(str2))
+    while index < len(str2):
+        for i in str2:
+            if str2[index].isalpha():
+                if str2[index - 1] != '+' or str2[index + 1] != '+':
+                    return 'false'
+                print(index)
+            index += 1
+        return 'true'
+
+
+print(SimpleSymbols('o+3=+s+'))
